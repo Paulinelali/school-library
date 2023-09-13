2e2da620bf93c322e31873d593217a5fcc86ed03
Ayomide <ayomidesunday31@gmail.com>
2023-09-13 11:42:38 +0100

    Decorate classes

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-class Person
+require './nameable'
+class Person < Nameable
   def initialize(age, name = 'Unkown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     @age > 18 || @parent_permission == true
+  end
+
+  def correct_name
+    @name
   end
 
   private
